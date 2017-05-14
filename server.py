dad672faa3e8349119fcd4dad55fff620edcda1a
moti <moti@practitest.com>
2017-05-15 00:16:03 +0700

    Return actual results

--- server.py
+++ server.py
@@ -9,16 +9,24 @@ from rq import Worker, Queue, Connection
 from searcher import search
 from worker import redistogo_conn
 
-
 queue = Queue(connection=redistogo_conn)
+redis_conn = redis.from_url(os.environ.get("REDIS_URL", "redis://localhost:6379/1"))
 
 class RequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        self.send_response(200)
+        request_path = self.path
+        print(request_path)
+        val = redis_conn.get(request_path[1:])
+        if val is None:
+            self.send_response(404)
+        else:
+            self.send_response(200)
+            self.send_header('Content-type','application/json')
+            self.end_headers()
+            self.wfile.write(val)
 
     def do_POST(self):
-        request_path = self.path
         request_headers = self.headers
         content_length = request_headers.getheaders('content-length')
         length = int(content_length[0]) if content_length else 0
