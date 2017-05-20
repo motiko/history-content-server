5e5badeae4df7424989736f07ee55673fdf1b0a9
moti <moti@practitest.com>
2017-05-21 03:15:05 +0700

    remove some remained scripts

--- searcher.py
+++ searcher.py
@@ -2,6 +2,7 @@ import requests
 import json
 import redis
 import os
+from requests.exceptions import RequestException
 from bs4 import BeautifulSoup
 
 def search(request_id, search_query, urls):
@@ -9,12 +10,17 @@ def search(request_id, search_query, urls):
     redis_conn.set(request_id, '{"status":"in_progress"}')
     result_urls = []
     for url in urls:
-        url_content = requests.get(url)
-        page_source = url_content.text
-        if search_query in page_source:
-            soup = BeautifulSoup(page_source,"html.parser")
-            if search_query in soup.get_text():
-                result_urls.append(url)
+        try:
+            url_content = requests.get(url)
+            page_source = url_content.text
+            if search_query in page_source:
+                soup = BeautifulSoup(page_source,"html.parser")
+                for script in soup(["script", "style"]):
+                    script.extract()
+                if search_query in soup.get_text():
+                    result_urls.append(url)
+        except RequestException:
+            print("Couldn't connect to: %s" % url)
     response_dict = {"original_query": search_query,
                 "found_urls":result_urls, "status": "finished"}
     redis_conn.set(request_id, json.dumps(response_dict))
